88b278fc903283f1256e9bf69808e3dae1eb06f0
Saša Tomislav Mataić <stmataic@gmail.com>
2025-02-14 12:09:24 +0100

    complete task 9

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -66,6 +66,21 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
   return res.status(200).json({ message: "Review added successfully" });
 });
 
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+  // Filter & delete the reviews based on the session username,
+  // so that a user can delete only his/her reviews and not other users’.
+  const { isbn } = req.params;
+  const { username } = req.user;
+
+  if (!books[isbn]) {
+    return res.status(404).json({ message: "Book not found" });
+  }
+
+  delete books[isbn].reviews[username];
+
+  return res.status(200).json({ message: "Review deleted successfully" });
+});
+
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
 module.exports.users = users;
